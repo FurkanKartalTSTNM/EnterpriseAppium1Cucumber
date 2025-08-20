c5a27ff9e337e4f64dc2525dec24faf1765557e4
Furkan Kartal <Furkan.kartal@testinium.com>
2025-08-20 09:50:28 +0300

    appium2

--- src/test/java/com/saha/cucumber/step/Steps.java
+++ src/test/java/com/saha/cucumber/step/Steps.java
@@ -1,10 +1,8 @@
 package com.saha.cucumber.step;
 
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.ios.IOSDriver;
-import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
@@ -13,17 +11,14 @@ import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 
 public class Steps {
 
     public static final String hubURL = "http://host.docker.internal:4444/wd/hub";
-    protected static AppiumDriver<MobileElement> driver;
-
+    protected static AppiumDriver driver; // <MobileElement> KALDIRILDI
 
     @Before
     public static void setUp() throws Exception {
@@ -31,20 +26,20 @@ public class Steps {
 
         if (!StringUtils.isEmpty(System.getProperty("key"))) {
             capabilities.setCapability("key", System.getProperty("key"));
-            if (System.getProperty("platform").equals("ANDROID")) {
-                capabilities
-                        .setCapability(AndroidMobileCapabilityType.APP_PACKAGE,
-                                "com.gratis.android");
 
-                capabilities
-                        .setCapability(AndroidMobileCapabilityType.APP_ACTIVITY,
-                                "com.app.gratis.ui.splash.SplashActivity");
-                capabilities.setCapability(CapabilityType.PLATFORM_NAME, Platform.ANDROID);
-                driver = new AndroidDriver<MobileElement>(new URL(hubURL), capabilities);
+            if ("ANDROID".equalsIgnoreCase(System.getProperty("platform"))) {
+                capabilities.setCapability("appium:appPackage", "com.gratis.android");
+                capabilities.setCapability("appium:appActivity", "com.app.gratis.ui.splash.SplashActivity");
+                capabilities.setCapability("platformName", "Android");
+
+                // <MobileElement> JENERİĞİ YOK
+                driver = new AndroidDriver(new URL(hubURL), capabilities);
             } else {
-                capabilities.setCapability(CapabilityType.PLATFORM_NAME, Platform.IOS);
-                capabilities.setCapability("bundleId", "com.pharos.Gratis");
-                driver = new IOSDriver<MobileElement>(new URL(hubURL), capabilities);
+                capabilities.setCapability("platformName", "iOS");
+                capabilities.setCapability("appium:bundleId", "com.pharos.Gratis");
+
+                // <MobileElement> JENERİĞİ YOK
+                driver = new IOSDriver(new URL(hubURL), capabilities);
             }
         }
     }
@@ -57,13 +52,8 @@ public class Steps {
         }
     }
 
-    /**
-     * @param seconds
-     */
     @Given("^Wait (\\d+) seconds$")
-    public void waitSeconds(int seconds) {
-        wait(seconds);
-    }
+    public void waitSeconds(int seconds) { wait(seconds); }
 
     @Given("^Element with id \"([^\"]*)\" is clicked$")
     public void clickElementById(String elementId) {
@@ -77,13 +67,8 @@ public class Steps {
         element.click();
     }
 
-
-
     public void wait(int seconds) {
-        try {
-            TimeUnit.SECONDS.sleep(seconds);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        try { TimeUnit.SECONDS.sleep(seconds); }
+        catch (InterruptedException e) { Thread.currentThread().interrupt(); }
     }
 }
